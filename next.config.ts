4c633eb1a46fbe598bf09ee81a064e7c2d564efa
Sanjit <dash.pranta145@gmail.com>
2026-09-16 17:57:27 +0600

    Files are updated

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,16 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    // Add aliases
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     };
+
+    // Also add modules resolution
+    config.resolve.modules = [path.resolve(__dirname, "src"), "node_modules"];
+
     return config;
   },
 };
